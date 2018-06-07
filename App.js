14cbba4c95320bb88674e8fe40e0a8f9358ea3aa
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-08 01:58:01 +0300

    fixed import

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { React, Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from '.src/components';
+import { Header } from '.src/components/Header';
 
 export default class App extends React.Component {
   render() {
@@ -13,7 +13,7 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
