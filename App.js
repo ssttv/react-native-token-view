4f51a9c67d59c434687bdb4a9a75eb26b8876792
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-08 03:51:52 +0300

    Reducers created

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { Header } from './src/components/';
+import Store from './src/Store';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Header />
-      </View>
+      <Provider store={ Store }>
+        <View>
+          <Header />
+        </View>
+      </Provider>
     );
   }
 }
