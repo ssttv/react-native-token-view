1606ccb5eb5bb4e1470014d5c9b8b21ea95944d2
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-09 23:46:39 +0300

    text properties introduced to the card

--- src/components/TokenCard.js
+++ src/components/TokenCard.js
@@ -5,8 +5,13 @@ import {
   StyleSheet
 } from 'react-native';
 
-const TokenCard = () => {
+const TokenCard = (props) => {
   return(
+    <View>
+      <Text>{props.symbol}</Text>
+      <Text>{props.price_usd}</Text>
+      <Text>{props.percent_change_24h}</Text>
+    </View>
 
   )
 }
