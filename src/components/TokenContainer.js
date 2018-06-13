7747bcf5d1b0c1882811aa733a50f7872c0a451a
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-13 19:04:25 +0300

    spinner import

--- src/components/TokenContainer.js
+++ src/components/TokenContainer.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
-import FetchTokenData from './../Actions/FetchTokenData'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, Text } from "react-native";
+import FetchTokenData from "./../Actions/FetchTokenData";
+import spinner from "react-bative-loading-spinner-overlay";
 
 class TokenContainer extends Component {
-
   componentDidMount() {
     this.props.FetchTokenData();
   }
@@ -12,33 +12,28 @@ class TokenContainer extends Component {
   renderTokenCards() {
     const { token } = this.props;
     console.log(token);
-    return token.data.map((token, index) =>
-    <TokenCard
-      key={index}
-      token_name={token.name}
-      symbol={token.symbol}
-      percent_change_24h={token.percent_change_24h}
-      percent_change_7d={token.percent_change_7d}
-    />
-  )
+    return token.data.map((token, index) => (
+      <TokenCard
+        key={index}
+        token_name={token.name}
+        symbol={token.symbol}
+        percent_change_24h={token.percent_change_24h}
+        percent_change_7d={token.percent_change_7d}
+      />
+    ));
   }
 
   render() {
-
     const { token } = this.props;
 
-    return (
-      <View>
-        {this.renderTokenCards()}
-      </View>
-    )
+    return <View>{this.renderTokenCards()}</View>;
   }
 }
 
 function mapStateToProps(state) {
   return {
     token: state.token
-  }
+  };
 }
 
-export default connect(mapStateToProps)(TokenContainer)
+export default connect(mapStateToProps)(TokenContainer);
